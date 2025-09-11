2a58b2ecdd77ccdc013503f080bfd99637818a85
Pablo Maldonado Presas <139907881+CybeR-LuduS@users.noreply.github.com>
2025-09-11 16:27:02 -0300

    Unificación de diseño Material UI y mejoras en AuthForm (#5)
    * feat: agregar soporte para Google Login y mejorar el formulario de autenticación, y consolidar el uso de Material UI de manera unificada
    
    * refactor: eliminar ClientProviders de las páginas Home y Profile para simplificar la estructura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-'use client';
-
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { theme } from '../theme/theme';
 import DashboardLayout from '../components/layout/DashboardLayout';
 import Dashboard from '../components/dashboard/Dashboard';
 
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <DashboardLayout>
-        <Dashboard />
-      </DashboardLayout>
-    </ThemeProvider>
+    <DashboardLayout>
+      <Dashboard />
+    </DashboardLayout>
   );
 }
