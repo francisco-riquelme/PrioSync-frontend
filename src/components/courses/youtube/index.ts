2b83847b028ca7a1c36aa91f3f0eaeb705a4fe00
Pablo-Maldonado-Presas <pianola89@gmail.com>
2025-10-04 10:35:29 -0300

    feat: agregar exportaciones de componentes YouTube
    - Crear barrel export para componentes de YouTube
    - Facilitar importación de YouTubeImport y CourseStructurePreview
    - Organizar estructura de módulos de importación

---
+++ src/components/courses/youtube/index.ts
@@ -0,0 +1,4 @@
+// Exportaciones de componentes de YouTube para importación de cursos
+
+export { YouTubeImport } from './YouTubeImport';
+export { CourseStructurePreview } from './CourseStructurePreview';
